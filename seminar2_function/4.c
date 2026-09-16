ce806367974a94bd0b86a8516e384a9950f28ac2
Борис Ларин <br-ek-sv@mail.ru>
2026-09-16 09:47:17 +0000

    Update 4.c

--- seminar2_function/4.c
+++ seminar2_function/4.c
@@ -10,8 +10,9 @@ int sum_of_digits(int a){
 }
 
 int sum_of_digits_rec(int a){
-        if(a == 0) return 0;
-        return a % 10 + sum_of_digits_rec(a / 10);
+        if(a == 0) 
+                return 0;
+        return a%10 + sum_of_digits_rec(a/10);
 }
 
 int main(){
